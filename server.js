367f21b59ebf465c8b6603f5c7723aa6f25bb158
Shahin M Hashim <shahinmhashim@gmail.com>
2023-12-16 23:17:28 +0530

    learned 1st method of sending html file response

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import http from 'http';
+import fs from 'fs';
 
 const port = 3000;
 const hostname = '127.0.0.1';
 
-const msg = 'Hello There' // to simply render text files we specify content type as text/plain
-const msgObj = { name: 'Shahin', status: 'test' } // to render objects we need to use json stringify
+// while sending responses we normally send html for the browser to render
+// so we don't simply send the html response as string, eg: res.end('<h1>Hello World</h1>');
+// instead we send the file that we need as the response using built-in fs module
+
+const htmlFile = fs.readFileSync('./index.html', 'utf8');
 
 const server = http.createServer((req, res) => {
-    // res.writeHead(200, { 'Content-Type': 'text/plain' })
-    // res.end(msg);
-
-    // to send objects as json response we need to convert it to json using JSON.stringify()
-    // res.writeHead(200, { 'Content-Type': 'application/json' })
-    // res.end(JSON.stringify(msgObj));
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(htmlFile);
 })
 
 server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
