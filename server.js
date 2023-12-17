6013a3ed764e8110037dac3c15137032cd860f8e
Shahin M Hashim <shahinmhashim@gmail.com>
2023-12-17 15:10:58 +0530

    learned the third approach of sending dynamic html file response

--- server.js
+++ server.js
@@ -4,27 +4,31 @@ import fs from 'fs';
 const port = 3000;
 const hostname = '127.0.0.1';
 
-// while res.end(htmlFile) helps, when it comes to large html documents this affects performance
-// greatly so its always best to use streams to read and write data streams using pipe
-// streams is simply used to read or write data in smaller chunks from the buffer and not entirely at once
-// pipes are used to read and write data streams in a more efficient way
-
-// const readHtmlFileStream = fs.createReadStream('./index.html', 'utf8');
-
-// if readHtmlFileStream is created outside the server handler:
-// The readHtmlFileStream is created once when the module is loaded. When the stream is piped using 
-// readHtmlFileStream.pipe(res), it starts reading the file from the beginning. However, 
-// on subsequent requests, the stream has already been consumed, and it won't "rewind" to the beginning 
-// automatically.
+// using streams and pipes are good for static files like images, videos, etc.
+// If your HTML document contains dynamic data that needs to be generated dynamically for each request, 
+// you'll typically need to use a templating engine or a mechanism to inject dynamic data into your HTML
+// before sending it as a response. Streaming the entire HTML file might not be suitable for cases where 
+// the content needs to be customized for each request.
 
 const server = http.createServer((req, res) => {
+    const name = 'Shahin';
+
     res.writeHead(200, { 'Content-Type': 'text/html' })
 
-    const readHtmlFileStream = fs.createReadStream('./index.html', 'utf8');
-    //make sure to read the stream here or each time u reload the stream is lost 
+    let indexHTML = fs.readFileSync('./index.html', 'utf8');
+    indexHTML = indexHTML.replace('{{name}}', name);
 
-    // Pipe the file stream directly to the response stream
-    readHtmlFileStream.pipe(res);
+    res.end(indexHTML);
 })
+
+/*
+    This method can work well for simpler scenarios and is suitable when you have a limited number of 
+    dynamic values to replace.
+
+    As your application grows or if you have more complex dynamic content, using a templating engine might 
+    become a more maintainable solution. Templating engines offer features like conditional statements, 
+    loops, and a cleaner syntax for injecting dynamic data into HTML. They are designed specifically for 
+    this purpose and can make your code more readable and extensible.
+*/
 
 server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
