ed04fe81e196971f677ba8f9e6e3a95782485e61
Shahin M Hashim <shahinmhashim@gmail.com>
2023-12-17 13:41:17 +0530

    learned a bit about streams in node

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
+import fs, { read } from 'fs';
+
 // streams - a sequence of data moved from one place to another over time
+// streams are used to read or write input or output sequences of data
 // a stream of data being transferred from server to client device
 
-// buffers - a temporary holding spot for data being moved from one place to another
-// they are used along with streams to process data in smaller chunks as they arrive instead of
-// waiting for the entire data. Eg: video streaming on you tube
+// 4 types of streams: readable, writable, duplex, transform
+// readable - can read data from a stream (fs.createReadStream())
+// writable - can write data to a stream (fs.createWriteStream())
+// duplex - can read and write to a stream. Eg: sockets 
+// transform - can modify the data as its being read and written. 
+// Eg: compression , write compressed data and read from decompressed data
 
-// if the network has the speed to transmit 10mb/s then in 1s you can easily download 10mb data
-// but what if the data is 100mb/s, then it will take 10s to download the entire data
-// so each second 10mb data is downloaded and stored in a buffer and then the buffer is emptied
-// and the next 10mb data is downloaded and stored in the buffer and so on till the whole 100mb
-// is transferred
+const readableStream = fs.createReadStream('./file1.txt', { encoding: 'utf-8', highWaterMark: 5 });
 
-// this is essential because if the data is not stored in a buffer then the entire data will have to be
-// downloaded and then stored in the memory which will take a lot of time and performance
+// streams extend from the event emitter class
+// readableStream emit an event we can can listen to
 
-// streams and buffers are used to process data in chunks instead of the entire data at once
+readableStream.on('data', chunk => console.log("Stream of data read from file1.txt: ", chunk));
 
-import { Buffer } from 'buffer';
+const writableStream = fs.createWriteStream('./file2.txt');
+readableStream.on('data', chunk => console.log("Data wrote to file2.txt", writableStream.write(chunk)));
 
-// buffer is a global object in nodejs
-// buffer is a temporary storage spot for a chunk of data that is being transferred from one place to another
-// the buffer is filled with data, then passed along and then the buffer is emptied
+// the reason the entire file content of file1.txt is written to file2.txt is because
+// the buffers used by streams have a default size of 64kb and the file1.txt is less than 64kb
+// the file1.txt has 14 characters so the size is 14 bytes which is way less than 64kb
+// so to set a limit to the size of the buffer we can use the highWaterMark option
 
-const buffer = Buffer.from('Hello World');
-console.log("Raw Buffer Data: ", buffer);
-// even though raw is binaries, it is converted to hexadecimals to prevent overflowing 
-// of 0's and 1's when displayed in the terminal
-console.log("JSON Buffer Data: ", buffer.toJSON());
-console.log("String Buffer Data: ", buffer.toString());
-
-buffer.write('Replaced');
-// replaces the buffer data with the new data but the size of the buffer remains the same,
-// so if the new data is larger than the buffer size of 8 bytes then the data will be truncated
-console.log("String Buffer After Replacing: ", buffer.toString());
-
-console.log("Default Buffer Length:", buffer.length); // Outputs the length of the buffer in bytes
-
-// Create a buffer with a specific size (e.g., 10 bytes)
-const newBuffer = Buffer.alloc(15).fill('New Buffer Data');
-console.log("New Buffer Data: ", newBuffer);
-console.log("New Buffer Length:", newBuffer.length);
+// in real life we read mostly large data's more than 64kb (mb and gb)
+// so its really important to set the highWaterMark option to a reasonable size
+// so that we don't run out of memory when reading large files and also to improve performance
