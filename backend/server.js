e9dbf2dcb5371f1493fda17fc3e7dcbf04c53973
Shahin M Hashim <shahinmhashim@gmail.com>
2024-01-20 22:33:25 +0530

    basic and advanced use of query in api endpoints

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,41 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => res.json(users));
 
+// app.get('/users/hello', (req, res) => res.send('Hello World'));
+// make sure to always add dynamic routes at the end else it will override other routes like above
+
+// querying data from the server using query parameters from the req.query object
+
+// find a user by username using find method which returns the first element that satisfies the condition
+app.get('/users/search', (req, res) => {
+    const { username } = req.query;
+    const user = users.find(user => user.username === username);
+    return user ? res.json(user) : res.status(200).send('<center><h1>Searched User Doesn\'t Exist</h1></center>');
+});
+
+// filter out users by a limit 
+app.get('/users/filter', (req, res) => {
+    const { limit } = req.query;
+    const limitedUsers = users.slice(0, Number(limit));
+    return limitedUsers ? res.json(limitedUsers) : res.status(200).send('<center><h1>Searched User Doesn\'t Exist</h1></center>');
+});
+
+// advanced search
+app.get('/users/advanced-search', (req, res) => {
+    const { username, limit } = req.query;
+    filteredUsers = username
+        ? users.filter(user => user.username.startsWith(username))
+        : (limit
+            ? users.slice(0, Number(limit))
+            : users
+        );
+
+    return filteredUsers ? res.json(filteredUsers) : res.status(200).send('<center><h1>Searched User Doesn\'t Exist</h1></center>');
+});
+
+// make sure to use return when sending back response based on condition else it may throw error incase
+// u send a response one after the other. so to avoid that use return to exit the function. 
+
 app.get('/users/:id', (req, res) => {
     const user = users.find(user => user.id === Number(req.params.id));
     user ? res.json(user) : res.status(404).send('<center><h1>User Doesn\'t Exist</h1></center>');
