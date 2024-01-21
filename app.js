d6db8d0cf1d3ecf4ff27d7ae0875026b8d1a043c
Shahin M Hashim <shahinmhashim@gmail.com>
2024-01-21 22:47:24 +0530

    learned controllers in express

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = 3000;
+
+// Middleware to parse JSON request body
+app.use(express.json());
+
+const usersRouter = require('./routes/usersRouter');
 
 app.get('/', (req, res) => res.send('Welcome to Express & Node JS Backend Server'));
 
-const userRouter = require('./routes/userRouter');
-
-app.use('/user', userRouter);
-// incase there is no base path, then we can use app.use(userRouter);
-// the '/user' is a base path, its like a prefix to all the paths in userRouter.
-// this middleware just appends this path to the userRouter paths and is handled by userRouter.
+app.use('/users', usersRouter);
 
 app.get('*', (req, res) => res.status(404).send('<center><h1>404, Page Not Found</h1></center>'));
 
