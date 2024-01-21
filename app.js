de9a85d64bf3ca4665b1f87db57c34cad26f0090
Shahin M Hashim <shahinmhashim@gmail.com>
2024-01-21 20:57:36 +0530

    working of put using express and insomnia api tool

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-// this is used to generate unique id's for each user
 
 const app = express();
 
 const PORT = 3000;
 
 app.get('/', (req, res) => res.send('Welcome to Express & Node JS Backend Server'));
-
-// while working with post method there are certain key things to know
-// by default express sends data in json format when using send method
-
-// req.params: This refers to route parameters in the URL of the request.
-// When defining routes in Express, you can specify placeholders in the URL, and the values for
-// these placeholders are stored in "req.params".For example, in the URL "/users/:userId",
-// the value for ":userId" would be accessible through "req.params.userId".
-
-// req.body: This refers to the data sent with a POST or PUT request.Typically, when sending data from
-// a form or through an API request, the data is included in the body of the request, and it is parsed and
-// made accessible through "req.body".
-
-// In Express.js, when handling a POST request, form data or JSON data sent in the request body would be 
-// accessed using req.body, while route parameters specified in the URL would be accessed using req.params.
 
 /* 
     Insomnia testing and debugging
@@ -33,38 +16,41 @@ app.get('/', (req, res) => res.send('Welcome to Express & Node JS Backend Server
     6. Incase of post,put,delete etc use the body tab to enter the data
 */
 
-app.post('/login/param/:uid', (req, res) => {
-    console.log("Request Params: ", req.params); // handles params like /login/123
-    res
-        .status(201)
-        .send('Param POST Request Successful');
-});
-
-app.post('/login/query/', (req, res) => {
-    console.log("Request Query: ", req.query); // handles query like /login/query?uid=123&pass=example&name=John
-    res
-        .status(201)
-        .send('Query POST Request Successful');
-});
-
-app.use(express.json());
-// handles body like {"uid": 123, "name": "John"}
-// this middleware is used to parse the body of the request and make it accessible through req.body
-// it's crucial to use this middleware before handling any POST requests instead of any JSON methods
-
-
-// Using Body is the most recommended and secure way of sending data
-
-const user = [];
-app.post('/login/body/', (req, res) => {
-    if (Object.keys(req.body).length === 0) { // Check if the request body is empty
-        return res.status(400).send('Error: Empty request body'); // Return a 400 Bad Request response for an empty body
+const users = [
+    {
+        id: 1,
+        name: 'John',
+    },
+    {
+        id: 2,
+        name: 'Jane',
+    },
+    {
+        id: 3,
+        name: 'Doe',
     }
+];
 
-    const id = uuidv4();
-    user.push({ id, ...req.body });
-    console.log("Data: ", user);
-    res.status(201).send('Body POST Request Successful');
+app.put('/update/user/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.query;
+    // console.log(req.query);
+    const user = users.find(user => user.id === parseInt(id));
+
+    if (user) {
+        user.name = name || user.name;
+        res.status(200).json({
+            success: true,
+            message: `User with id ${id} updated successfully`,
+            data: users
+        });
+    } else {
+        res.status(404).json({
+            success: false,
+            message: `User with id ${id} doesn't exist`,
+            data: users
+        });
+    }
 });
 
 app.get('*', (req, res) => res.status(404).send('<center><h1>404, Page Not Found</h1></center>'));
