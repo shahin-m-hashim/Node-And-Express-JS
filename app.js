d0c980ce744d749dbfd57c31ec55406d1bc098f6
Shahin M Hashim <shahinmhashim@gmail.com>
2024-01-21 11:44:49 +0530

    2nd way  of using middleware

--- app.js
+++ app.js
@@ -16,13 +16,32 @@ const logger = (req, res, next) => {
     // or u can pass the request to the next middleware. 
 }
 
-app.get('/about', logger, (req, res) => {
-    res.send('About Page');
-});
+// app.get('users/user/about', logger, (req, res) => res.send('About Page'));
 
-app.get('/contact', logger, (req, res) => {
-    res.send('Contact Page');
-});
+// app.get('users/user/contact', logger, (req, res) => res.send('Contact Page'));
+
+// app.get('users/user/profile', logger, (req, res) => res.send('Profile Page'));
+
+// app.get('users/user/login', logger, (req, res) => res.send('Login Page'));
+
+// as u can see, we have used the logger middleware in all the routes.
+// this is very inefficient as we can see the path 'users/user/' repeating in all the routes.
+// so we can use the app.use() method to specify the path for the middleware.
+
+
+app.use('/users/user', logger);
+// this will intercept all the requests that start with /users/user
+// this is order specific like middleware fn's. if u place these after the routes, it will not work.
+// this middleware will intercept all the requests that start with /users/user
+
+app.get('/about', (req, res) => res.send('About Page'));
+
+app.get('/users/user/contact', (req, res) => res.send('Contact Page'));
+
+app.get('/users/user/profile', (req, res) => res.send('Profile Page'));
+
+app.get('/login', (req, res) => res.send('Login Page'));
+
 
 // this handles invalid routes
 app.get('*', (req, res) => res.status(404).send('<center><h1>404, Page Not Found</h1></center>'));
