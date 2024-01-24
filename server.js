f1a6e9b601c82c2d490565909d6f585a0ef6e562
Shahin M Hashim <shahinmhashim@gmail.com>
2024-01-24 16:09:56 +0530

    creating and dropping databases from the server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Require MongoDB language driver
-const { MongoClient, ObjectId, Collection } = require("mongodb");
+const { MongoClient } = require("mongodb");
 require('dotenv').config();
 
 // Set uri of connection string.
@@ -10,34 +10,38 @@ const uri = process.env.MONGODB_URI;
 // Create the MongoClient instance, this connects to a cluster or standalone server
 const client = new MongoClient(uri);
 
-// Database Name
-const dbName = "students";
+console.log('Connecting to the server or cluster...');
 
-console.log('Connecting to the database...');
+// node.js driver and mongo-shell are different, have similar syntax but not exactly the same.
+// so use - https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#find
+// https://www.mongodb.com/docs/drivers/node/current/
 
 async function Run() {
     try {
-        // Connect to the MongoDB server
+        // Connect to the MongoDB cluster or server
         await client.connect();
 
-        console.log('Connected to the database');
+        // Database Name
+        const dbName = "test";
 
         // Access your database and your collection
         const database = client.db(dbName);
 
-        // node.js driver and mongo-shell are different, have similar syntax but not exactly the same.
-        // so use - https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#find
-        // https://www.mongodb.com/docs/drivers/node/current/
+        console.log('Connected to the database', database.databaseName);
+        // console.log("Database stats:", await database.stats());
+        const collections = await database.listCollections().toArray();
+        collections.length > 0 ?
+            console.log("Collections in the database:", collections.map(collection => collection.name)) :
+            console.log("No collections in the database");
 
-        console.log('Creating collections in the database...');
+        // create a collection
+        let result = await database.createCollection('collectionTest');
+        console.log("Created a collection: ", result.collectionName);
 
-        // Create a collection
-        let result = await database.createCollection("test");
-        console.log(result.collectionName, 'has been created in the database', result.dbName);
 
-        // Drop a collection
-        // result = await database.dropCollection("test");
-        // console.log("Collection test has been dropped from the database:", result);
+        // drop the database
+        result = await database.dropDatabase();
+        console.log("Dropped the database: ", result);
 
     } catch (err) {
         console.error('Connection Error Occurred:', err.message);
