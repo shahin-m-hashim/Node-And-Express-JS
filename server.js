700d83530775e8206950625c897c2e9da6d7b11c
Shahin M Hashim <shahinmhashim@gmail.com>
2024-01-24 11:23:17 +0530

    understand env files, create a collection, inserting single and multiple documents

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // Require MongoDB language driver
 const { MongoClient } = require("mongodb");
+require('dotenv').config();
 
 // Set uri of connection string.
-const uri = "mongodb://127.0.0.1:27017"
+const uri = process.env.MONGODB_URI;
+
+// process.env will look for the .env file and load the variables into the environment
 
 // Create the MongoClient instance, this connects to a cluster or standalone server
 const client = new MongoClient(uri);
@@ -12,7 +15,7 @@ const dbName = "students";
 
 console.log('Connecting to the database...');
 
-async function connectToDatabase() {
+async function Run() {
     try {
         // Connect to the MongoDB server
         await client.connect();
@@ -22,7 +25,45 @@ async function connectToDatabase() {
         // Access your database
         const database = client.db(dbName);
 
-        console.log('Database:', database);
+        // Access your collection
+        const collection = database.collection('cse');
+
+        // single line collection syntax
+        // const collection = client.db(dbName).collection('cse');
+
+        const document = { name: 'John Doe', major: 'Computer Science', year: 2023 };
+
+        const multipleDocuments = [
+            {
+                name: 'Sam Daniel',
+                age: 30,
+                profession: 'Engineer'
+            },
+            {
+                name: 'Jane Smith',
+                age: 28,
+                profession: 'Designer'
+            },
+            {
+                name: 'Alex Johnson',
+                age: 35,
+                profession: 'Manager'
+            }
+        ]
+
+        // console.log('Database:', database);
+        // console.log('Collection:', collection);
+
+        const insertOneStatus = await collection.insertOne(document);
+        console.log(`Document inserted with the _id: ${insertOneStatus.insertedId}`);
+
+        const insertManyStatus = await collection.insertMany(multipleDocuments);
+        console.log(`${insertManyStatus.insertedCount} documents were inserted`);
+
+        /*
+            It's quite straightforward to use the MongoDB Node.js driver to perform operations 
+            such as insertOne, insertMany, find, and more.
+        */
 
     } catch (err) {
         console.error('Connection Error Occurred:', err.message);
@@ -32,4 +73,4 @@ async function connectToDatabase() {
     }
 }
 
-connectToDatabase();
+Run();
